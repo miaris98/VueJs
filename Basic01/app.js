0825df4dca82a17e01b290bb204c889e7be63407
dionushs <miaris54@gmail.com>
2021-09-24 10:49:51 +0300

    callable methods from html elements

--- Basic01/app.js
+++ Basic01/app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
       courseGoal:'whatever',
       link:'https://github.com/miaris98'  
     };
+  },
+  methods: {
+    outputGoal(){
+      const randNum=Math.random();
+      if(randNum<0.5){
+        return 'whatever1'
+      }else{
+        return 'whatever2'
+      }
+    }  
   }  
 });
 
