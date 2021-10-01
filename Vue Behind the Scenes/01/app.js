1482c1f2ad0a7f23f4e5de0cbcc9ed903ce90dbf
dionushs <miaris54@gmail.com>
2021-10-01 14:24:38 +0300

    added foundation and an intepretation of Vue in vanilla javascipt

---
+++ Vue Behind the Scenes/01/app.js
@@ -0,0 +1,42 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount('#app');
+
+//vanilla javascript
+
+let message="hello";
+let Lmessage=message+"world";
+console.log(Lmessage);
+message="Hello!!!";
+console.log(Lmessage);// it is not printing the updated value
+
+
+const data ={
+  message:'Hello!',
+  longme:'hello world',
+},
+const handler = {
+  set(target,key,value){
+    if(key==='message'){
+      target.longme=value+'world!';
+    }
+    target.message=value;
+  }
+}
+const proxy = new Proxy(data,handler);
+proxy.message="Hello!!!";
