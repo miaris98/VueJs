7a36d6e67426d08a82f83d720f023a9326bcef97
dionushs <miaris54@gmail.com>
2021-10-02 15:02:16 +0300

    added component and template

--- Introduction To Componets/01/app.js
+++ Introduction To Componets/01/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      details: false,
       friends: [
         {
           id: "1",
@@ -18,10 +17,35 @@ const app = Vue.createApp({
       ],
     };
   },
+});
+
+app.component("friend-contact", {
+  template: `
+    <li>
+    <h2>{{friend.name}}</h2>
+    <button @click = "toggleDetails">{{details ? 'HIDE' : 'SHOW'}} Details</button>
+    <ul v-if = "details">
+      <li><strong>Phone:</strong>{{friend.phone}}</li>
+      <li><strong>Email:</strong>{{friend.email}}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      details: false,
+      friend: {
+        id: "1",
+        name: "Whatever",
+        phone: "210 321 0123 231",
+        email: "whatever@somemail.com",
+      },
+    };
+  },
   methods: {
     toggleDetails() {
-        this.details= !this.details;
+      this.details = !this.details;
     },
   },
-});
+}); //tag name of "html" element of component
+
 app.mount("#app");
