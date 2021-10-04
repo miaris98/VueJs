bd2a4b84b5d1dbc382289d8757faf2226a977a1b
dionushs <miaris54@gmail.com>
2021-10-04 15:11:34 +0300

    added baseline for second project on components

---
+++ Components/01/src/components/FriendContact.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ details ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="details">
+      <li>
+        <strong>Phone:</strong>
+        {{ friend.phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ friend.email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [
+    'name',
+    'phoneNumber',
+    'emailAddress', 
+  ],
+  data() {
+    return {
+      details:false,
+      friend: {
+          id: "1",
+          name: "whatever1",
+          phone: "101010101010",
+          email: "some@mail.com",
+        },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.details = !this.details;
+    }
+  }
+};
+</script>
