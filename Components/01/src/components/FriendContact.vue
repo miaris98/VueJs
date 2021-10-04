fcd3092711fd5f19351852aa580af36b9e5f0c74
dionushs <miaris54@gmail.com>
2021-10-04 15:55:58 +0300

    added more props details

--- Components/01/src/components/FriendContact.vue
+++ Components/01/src/components/FriendContact.vue
@@ -1,7 +1,7 @@
 <template>
   <li>
-    <h2>{{ name }}{{friendIsFav==='1' ? '(Favorite)' : ''}}</h2>
-    <button @click="changeFav"> Change Favorite</button> 
+    <h2>{{ name }}{{ friendIsFav === "1" ? "(Favorite)" : "" }}</h2>
+    <button @click="changeFav">Change Favorite</button>
     <button @click="toggleDetails">
       {{ details ? "Hide" : "Show" }} Details
     </button>
@@ -20,7 +20,26 @@
 
 <script>
 export default {
-  props: ["name", "phoneNumber", "emailAddress","isFav"],
+  //props: ["name", "phoneNumber", "emailAddress","isFav"],
+  props:{
+    name:{
+      type:String,
+      required:true,
+    } ,
+    phoneNumber:{
+      type:String,
+      required:true,
+    },
+    emailAddress:{
+      type:String,
+      required:true,
+    },
+    isFav:{
+      type:String,
+      required:false,
+      default:'0'//it can be a fuction and you can have a validator
+    },
+  },
   data() {
     return {
       details: false,
@@ -30,16 +49,16 @@ export default {
         phone: "101010101010",
         email: "some@mail.com",
       },
-      friendIsFav:this.isFav,
+      friendIsFav: this.isFav,
     };
   },
   methods: {
     toggleDetails() {
       this.details = !this.details;
     },
-    changeFav(){
-      this.friendIsFav= !this.friendIsFav;
-    }
+    changeFav() {
+      this.friendIsFav = !this.friendIsFav;
+    },
   },
 };
 </script>
