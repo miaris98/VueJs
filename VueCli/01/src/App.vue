a5e5bcb50604992710f01d451f4d387749e4657b
dionushs <miaris54@gmail.com>
2021-10-04 11:02:52 +0300

    added componets

---
+++ VueCli/01/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li><friend-contact></friend-contact></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue';
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "whatever1",
+          name: "whatever1",
+          phone: "110101001011",
+          email: "whatever1@somemailserver.com",
+        },
+        {
+          id: "whatever2",
+          name: "whatever2",
+          phone: "110101001011",
+          email: "whatever2@somemailserver.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ VueCli/01/src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ friend.phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ friend.email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "whatever1",
+        name: "whatever1",
+        phone: "010101010101",
+        email: "whatever1@server.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
