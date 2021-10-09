9d3c8e33f39f6d8012e46fdeb1d0e5c78b8048fb
dionushs <miaris54@gmail.com>
2021-10-09 18:21:34 +0300

    added Error hadling to the button

---
+++ Advanced Components/02/src/components/ErrorAlert.vue
@@ -0,0 +1,18 @@
+<template>
+    <dialog open>
+        <slot></slot>
+    </dialog>
+</template>
+
+<style scoped>
+dialog {
+    margin:0;
+    position: fixed;
+    top: 20vh;
+    left: 30%;
+    width: 40%;
+    background-color: wheat;
+    box-shadow:0 2px 8px darkcyan;
+    padding:1rem;
+}
+</style>
--- Advanced Components/02/src/components/ManageGoals.vue
+++ Advanced Components/02/src/components/ManageGoals.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsvalid">
+        <h2>Input is invalid</h2>
+        <button @click="confirm">Okay</button>
+    </error-alert>
   </div>
 </template>
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+    components:{
+        ErrorAlert
+    },
+    data(){
+        return{
+            inputIsvalid: false
+        }
+    },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if(enteredValue === ''){
+          this.inputIsvalid=true;
+      }
+    },
+    confirm(){
+        this.inputIsvalid=false;
+    }
+  },
+};
+</script>
