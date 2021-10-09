4570acdebb7e85c9beca3f12fe307bd85821623b
dionushs <miaris54@gmail.com>
2021-10-09 15:55:28 +0300

    added dynimically change of components with the keyword component is

---
+++ Advanced Components/02/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <!-- <TheHeader /> -->
+    <button @click="setSelected('active-goals')">Active Goals</button>
+    <button @click="setSelected('manage-goals')">Manage Goals</button>
+    <component :is="selectedComponent"></component>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+//import BadgeList from './components/BadgeList.vue';
+//import UserInfo from './components/UserInfo.vue';
+//import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals';
+import ManageGoals from './components/ManageGoals';
+
+export default {
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
+
+  },
+  data() {
+    return {
+      selectedComponent:'active-goals',
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+  methods: {
+    setSelected(cmp){
+      this.selectedComponent = cmp;
+    }
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
