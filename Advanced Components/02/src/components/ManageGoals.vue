97c5f388bef865b2c73c05504985bc0f234b00a9
dionushs <miaris54@gmail.com>
2021-10-09 18:24:11 +0300

    added teleportaton for semantic correct DOM

--- Advanced Components/02/src/components/ManageGoals.vue
+++ Advanced Components/02/src/components/ManageGoals.vue
@@ -3,34 +3,36 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
-    <error-alert v-if="inputIsvalid">
+    <teleport to="body">
+      <error-alert v-if="inputIsvalid">
         <h2>Input is invalid</h2>
         <button @click="confirm">Okay</button>
-    </error-alert>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 <script>
 import ErrorAlert from "./ErrorAlert.vue";
 
 export default {
-    components:{
-        ErrorAlert
-    },
-    data(){
-        return{
-            inputIsvalid: false
-        }
-    },
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsvalid: false,
+    };
+  },
   methods: {
     setGoal() {
       const enteredValue = this.$refs.goal.value;
-      if(enteredValue === ''){
-          this.inputIsvalid=true;
+      if (enteredValue === "") {
+        this.inputIsvalid = true;
       }
     },
-    confirm(){
-        this.inputIsvalid=false;
-    }
+    confirm() {
+      this.inputIsvalid = false;
+    },
   },
 };
 </script>
