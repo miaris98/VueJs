b8280a3eaba5a24e2b40fc727cc2eeba6c6de181
dionushs <miaris54@gmail.com>
2021-10-12 18:40:21 +0300

    added loading caption

--- Sending Http Requests/01/src/components/survey/UserExperiences.vue
+++ Sending Http Requests/01/src/components/survey/UserExperiences.vue
@@ -7,7 +7,8 @@
           >Load Submitted Experiences</base-button
         >
       </div>
-      <ul>
+      <p v-if="isLoading">LOADING...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -28,11 +29,13 @@ export default {
   },
   data() {
     return {
-      results: []
+      results: [],
+      isLoading:false,
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading=true;
       fetch('https://vue-http-3981a-default-rtdb.firebaseio.com/surveys.json')
         .then((response) => { // fuction where this refers to the same element it refers out of the function scope
           if (response.ok) {
@@ -40,6 +43,7 @@ export default {
           }
         })
         .then((data) => {
+          this.isLoading=false;
           const results = [];
           for (const id in data) {
             results.push({
