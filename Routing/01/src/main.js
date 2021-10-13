734b83c76821021503ce5b3221eb5bf6f3d5a524
dionushs <miaris54@gmail.com>
2021-10-13 20:00:07 +0300

    added notfound route

--- Routing/01/src/main.js
+++ Routing/01/src/main.js
@@ -4,12 +4,15 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams'},
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers,props:true } //carefull this syntax can match to all link so if you want another component it should be above this line
+    { path: '/teams/:teamId', component: TeamMembers,props:true }, //carefull this syntax can match to all link so if you want another component it should be above this line
+    {path: '/:catch(.*)', component:NotFound},
   ],
   linkActiveClass: 'router-link-active'
 });
