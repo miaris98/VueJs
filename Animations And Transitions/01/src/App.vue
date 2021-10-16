513012c45e42901a7d60ee736ae70c54471aef3e
dionushs <miaris54@gmail.com>
2021-10-16 17:48:53 +0300

    added styling with javascript

--- Animations And Transitions/01/src/App.vue
+++ Animations And Transitions/01/src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="container">
-    <div class="block" :class="{ Ani: animatedBl }"></div>
-    <button @click="animateB">Animate</button>
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition name="Para" @before-enter="beforeEnter"
-      ><!-- you can name each step ie enter-to-class="some_name" -->
-      <p v-if="isP_visible">Now you see me</p>
+    <transition
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @enter-cancelled="enterCancelled"
+      @leave-cancelled="leaveCancelled"
+    >
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
-    <button @click="pVisibile">Toggle P</button>
-    <div class="container">
-      <transition name="fade-button" mode="out-in">
-      <button @click="show" v-if="!buttonVisibility">Show</button>
-      <button @click="hide" v-else>Hide</button>
-      </transition>
-    </div>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
   </div>
   <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -23,42 +32,92 @@
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
-</template>
+</template>  
 
 <script>
 export default {
   data() {
     return {
+      animatedBlock: false,
       dialogIsVisible: false,
-      animatedBl: false,
-      isP_visible: false,
-      buttonVisibility: false
+      paraIsVisible: false,
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null
     };
   },
   methods: {
+    enterCancelled(el) {
+      console.log(el);
+      clearInterval(this.enterInterval);
+    },
+    leaveCancelled(el) {
+      console.log(el);
+      clearInterval(this.leaveInterval);
+    },
+    beforeEnter(el) {
+      console.log('beforeEnter');
+      console.log(el);
+      el.style.opacity = 0;
+    },
+    enter(el, done) {
+      console.log('enter');
+      console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter(el) {
+      console.log('afterEnter');
+      console.log(el);
+    },
+    beforeLeave(el) {
+      console.log('beforeLeave');
+      console.log(el);
+      el.style.opacity = 1;
+    },
+    leave(el, done) {
+      console.log('leave');
+      console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave(el) {
+      console.log('afterLeave');
+      console.log(el);
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
+    animateBlock() {
+      this.animatedBlock = true;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
+    },
     showDialog() {
       this.dialogIsVisible = true;
-    },
-    pVisibile() {
-      this.isP_visible = !this.isP_visible;
     },
     hideDialog() {
       this.dialogIsVisible = false;
     },
-    animateB() {
-      this.animatedBl = true;
-    },
-    show() {
-      this.buttonVisibility = true;
-    },
-    hide() {
-      this.buttonVisibility = false;
-    },
-    beforeEnter(el){
-
-      console.log(el);//enter method is used as active class
-    },
-  }
+  },
 };
 </script>
 
@@ -91,7 +150,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  /*transition: transform 0.3s ease-out;*/
+  /* transition: transform 0.3s ease-out; */
 }
 .container {
   max-width: 40rem;
@@ -104,66 +163,40 @@ button:active {
   border: 2px solid #ccc;
   border-radius: 12px;
 }
-.Ani {
-  /*transform: translateX(-50px);*/
-  animation: slide-fade 0.5s ease-out forwards;
+.animate {
+  /* transform: translateX(-150px); */
+  animation: slide-fade 0.3s ease-out forwards;
 }
 
-.Para-enter-from {
-  opacity: 0;
-  transform: translateY(-30px) scale(0.9);
-}
-
-.Para-enter-active {
-  transition: all 0.3s ease-out;
-}
-
-.Para-enter-to {
-  opacity: 1;
-  transform: translateY(0) scale(1);
-}
-
-.Para-leave-from {
-  opacity: 1;
-  transform: translateY(0) scale(1);
-}
-
-.Para-leave-active {
-  transition: all 0.3s ease-in;
-}
-
-.Para-leave-to {
-  opacity: 0;
-  transform: translateY(-30px) scale(0.9);
-}
-
-.fade-button-enter-from
-.fade-button-leave-to{
+.fade-button-enter-from,
+.fade-button-leave-to {
   opacity: 0;
 }
 
-.fade-button-enter-active{
-  transition: opacity 0.3s ease-out ;
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
 }
 
-.fade-button-leave-active{
-  transition: opacity 0.3s ease-in ;
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
 }
 
-.fade-button-enter-to
-.fade-button-leave-from{
+.fade-button-enter-to,
+.fade-button-leave-from {
   opacity: 1;
 }
 
-@keyframes slide-fade {
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
-  69% {
-    transform: translateX(120px) scale(0.9);
-  }
-  100% {
+
+  70% {
     transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
   }
 }
 </style>
--- Animations And Transitions/01/src/components/BaseModal.vue
+++ Animations And Transitions/01/src/components/BaseModal.vue
@@ -1,16 +1,16 @@
 <template>
   <div v-if="open" class="backdrop" @click="$emit('close')"></div>
-  <transition name="custom">
-  <dialog open v-if="open">
-    <slot></slot>
-  </dialog>
+  <transition name="modal">
+    <dialog open v-if="open">
+      <slot></slot>
+    </dialog>
   </transition>
 </template>
 
 <script>
 export default {
-  props: [ 'open' ],
-  emits: ['close']
+  props: ['open'],
+  emits: ['close'],
 };
 </script>
 
@@ -37,25 +37,26 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
-  /*animation: closing-dialog 0.3s ease-out forwards;*/
+  /* animation: modal 0.3s ease-out forwards; */
 }
 
-@keyframes closing-dialog {
+.modal-enter-active {
+  animation: modal 0.3s ease-out;
+}
+
+.modal-leave-active {
+  animation: modal 0.3s ease-in reverse;
+}
+
+@keyframes modal {
   from {
     opacity: 0;
     transform: translateY(-50px) scale(0.9);
   }
+
   to {
     opacity: 1;
-    transform: translateY(0px) scale(1);
+    transform: translateY(0) scale(1);
   }
-}
-
-.custom-enter-active {
-  animation: closing-dialog 0.3s ease-out ;
-}
-
-.custom-leave-active{
-  animation: closing-dialog 0.3s ease-in reverse;
 }
 </style>
