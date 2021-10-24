f0d85a4f8229042955571aeadc2217fda927111b
dionushs <miaris54@gmail.com>
2021-10-24 20:54:39 +0300

    added requests

--- Coaches/01/src/pages/requests/ContactCoach.vue
+++ Coaches/01/src/pages/requests/ContactCoach.vue
@@ -1,16 +1,16 @@
 <template>
   <form @submit.prevent="submitForm">
     <div class="form-control">
-      <label for="email"> email </label>
+      <label for="email">Your E-Mail</label>
       <input type="email" id="email" v-model.trim="email" />
     </div>
     <div class="form-control">
-      <label for="message"> Message </label>
-      <textarea id="message" rows="5" v-model.trim="message"></textarea>
+      <label for="message">Message</label>
+      <textarea rows="5" id="message" v-model.trim="message"></textarea>
     </div>
-    <p class="errors" v-if="!formIsValid">Enter valid email and message</p>
+    <p class="errors" v-if="!formIsValid">Please enter a valid email and non-empty message.</p>
     <div class="actions">
-      <base-button>Send message</base-button>
+      <base-button>Send Message</base-button>
     </div>
   </form>
 </template>
@@ -21,16 +21,29 @@ export default {
     return {
       email: '',
       message: '',
-      formIsValid: true
+      formIsValid: true,
     };
   },
-  submitForm() {
-    this.formIsValid = true;
-    if (this.email === '' || !this.email.includes('@') || this.message === '') {
-      this.formIsValid = false;
-      return;
-    }
-  }
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.message === ''
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+      this.$store.dispatch('requests/contactCoach', {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.id
+      });
+      this.$router.replace('/coaches');
+
+    },
+  },
 };
 </script>
 
