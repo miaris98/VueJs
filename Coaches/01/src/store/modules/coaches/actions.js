46b00d20d046fcd333cd6fd615f389454ef305dd
dionushs <miaris54@gmail.com>
2021-10-25 21:34:17 +0300

    added database storing of coaches

--- Coaches/01/src/store/modules/coaches/actions.js
+++ Coaches/01/src/store/modules/coaches/actions.js
@@ -1,14 +1,26 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas
     };
+    const response=await fetch(`https://coaches-f4be9-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,{
+    method: 'PUT',
+    body: JSON.stringify(coachData),
+    });
+    //const responseData = await response.json();
 
-    context.commit('registerCoach', coachData);
+    if(!response.ok){
+      //error
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id:userId
+    });
   }
 };
