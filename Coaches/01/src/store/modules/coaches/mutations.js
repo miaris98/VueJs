e8abce5e4b410ff43fabc8d6ce2c4a568922ae3b
dionushs <miaris54@gmail.com>
2021-10-26 15:54:58 +0300

    added spinner and error handling

--- Coaches/01/src/store/modules/coaches/mutations.js
+++ Coaches/01/src/store/modules/coaches/mutations.js
@@ -2,8 +2,7 @@ export default {
   registerCoach(state, payload) {
     state.coaches.push(payload);
   },
-  
-  setCoaches(state,payload){
+  setCoaches(state, payload) {
     state.coaches = payload;
   }
 };
