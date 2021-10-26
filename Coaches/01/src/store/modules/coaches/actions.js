ec465fe86bb106b427235e266abbe2e72dfa66d8
dionushs <miaris54@gmail.com>
2021-10-26 15:39:42 +0300

    added fetching from database to local

--- Coaches/01/src/store/modules/coaches/actions.js
+++ Coaches/01/src/store/modules/coaches/actions.js
@@ -8,19 +8,45 @@ export default {
       hourlyRate: data.rate,
       areas: data.areas
     };
-    const response=await fetch(`https://coaches-f4be9-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,{
-    method: 'PUT',
-    body: JSON.stringify(coachData),
-    });
+    const response = await fetch(
+      `https://coaches-f4be9-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData)
+      }
+    );
     //const responseData = await response.json();
 
-    if(!response.ok){
+    if (!response.ok) {
       //error
     }
 
     context.commit('registerCoach', {
       ...coachData,
-      id:userId
+      id: userId
     });
+  },
+
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://coaches-f4be9-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      //error handling
+    }
+    const coaches =[];
+    for (const key in responseData){
+      const coach ={
+      id:key,  
+      firstName:responseData[key].firstName,
+      lastName:responseData[key].lastName,
+      description: responseData[key].description,
+      hourlyRate:responseData[key].hourlyRate,
+      areas:responseData[key].areas
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches',coaches);
   }
 };
