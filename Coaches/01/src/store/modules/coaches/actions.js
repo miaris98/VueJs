e49a9f5a3cde1ba2cd279cf608c1da87626a1bb8
dionushs <miaris54@gmail.com>
2021-10-27 16:51:20 +0300

    changed fetching of data to be done every minute

--- Coaches/01/src/store/modules/coaches/actions.js
+++ Coaches/01/src/store/modules/coaches/actions.js
@@ -27,7 +27,10 @@ export default {
     });
   },
 
-  async loadCoaches(context) {
+  async loadCoaches(context,payload) {
+    if(!payload.forceRefresh && !context.getters.shouldUpdate){
+      return ;
+    }
     const response = await fetch(
       `https://coaches-f4be9-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
     );
@@ -49,5 +52,6 @@ export default {
       coaches.push(coach);
     }
     context.commit('setCoaches',coaches);
+    context.commit('setFetchTimestamp');
   }
 };
