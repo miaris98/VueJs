c6f246227409236077bc6900d6a82e10ca4e8239
dionushs <miaris54@gmail.com>
2021-11-03 21:19:12 +0200

    added localstorage for login

--- Coaches/01/src/App.vue
+++ Coaches/01/src/App.vue
@@ -13,46 +13,49 @@ import TheHeader from './components/layout/TheHeader.vue';
 export default {
   components: {
     TheHeader
-  }  
-}
+  },
+  created() {
+    this.$store.dispatch('tryLogin');
+  }
+};
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
 * {
   box-sizing: border-box;
 }
 
 html {
-  font-family: "Roboto", sans-serif;
+  font-family: 'Roboto', sans-serif;
 }
 
 body {
   margin: 0;
 }
 
-.route-enter-from{
- opacity: 0;
- transform: translateY(-30px);
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
 }
 
-.route-leave-to{
-opacity: 0;
- transform: translateY(30px);
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 
-.route-enter.active{
- transition: all 0.25s ease-out;
+.route-enter.active {
+  transition: all 0.25s ease-out;
 }
 
-.route-leave-active{
- transition: all 0.25s ease-in;
+.route-leave-active {
+  transition: all 0.25s ease-in;
 }
 
 .route-enter-to,
-.route-leave-from{
- opacity: 1;
- transform: translateY(0);
+.route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
 }
 </style>
--- Coaches/01/src/store/modules/auth/actions.js
+++ Coaches/01/src/store/modules/auth/actions.js
@@ -1,63 +1,68 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCkxI5H3-9PMaPL-1KO6SjcBmtk-fpJJW0',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(responseData.message || 'Failed to authenticate.');
-      throw error;
-    }
-
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
     });
   },
   async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCkxI5H3-9PMaPL-1KO6SjcBmtk-fpJJW0',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
+    });
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCkxI5H3-9PMaPL-1KO6SjcBmtk-fpJJW0';
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCkxI5H3-9PMaPL-1KO6SjcBmtk-fpJJW0';
+    }
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
     const responseData = await response.json();
 
     if (!response.ok) {
       console.log(responseData);
-      const error = new Error(responseData.message || 'Failed to authenticate.');
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.'
+      );
       throw error;
     }
+    localStorage.setItem('token',responseData.idToken);
+    localStorage.setItem('userId',responseData.localId);
 
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
-    });
-  },
-  logout(context){
+
     context.commit('setUser',{
-      token:null,
-      userId:null,
-      tokenExpiration:null,
+      token:responseData.idToken,
+      userId:responseData.localId,
+      tokenExpiration:responseData.expiresIn
     })
   },
+  tryLogin(context){
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    if(token && userId){
+      context.commit('setUser',{
+        token:token,
+        userId:userId,
+        tokenExpiration:null
+      })
+    }
+  },
+  logout(context) {
+    context.commit('setUser', {
+      token: null,
+      userId: null,
+      tokenExpiration: null
+    });
+  }
 };
