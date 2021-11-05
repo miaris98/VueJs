15333b5a50e141e9cce7ecce21567636f29f599a
dionushs <miaris54@gmail.com>
2021-11-05 18:06:12 +0200

    added ref

--- Composition API/01/src/App.vue
+++ Composition API/01/src/App.vue
@@ -5,12 +5,23 @@
 </template>
 
 <script>
+import {ref} from 'vue';
 export default {
-  data() {
-    return {
-      userName: 'Maximilian',
+  setup(){
+    const uName = ref('Dennis');
+    setTimeout(function (){
+      uName.value='dennis';
+    } ,2000)
+    return{
+      userName:uName,
+
     };
-  },
+  }
+  // data() {
+  //   return {
+  //     userName: 'Dennis',
+  //   };
+  // },
 };
 </script>
 
