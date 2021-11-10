fe1ef66e6faefd09a19068f86fea09fc6954729b
dionushs <miaris54@gmail.com>
2021-11-10 15:45:37 +0200

    added onMounted

--- Composition API/01/src/components/UserData.vue
+++ Composition API/01/src/components/UserData.vue
@@ -4,15 +4,19 @@
 </template>
 
 <script>
-import {computed , inject} from 'vue';
+import {computed , inject ,onMounted } from 'vue';
 export default {
     props:['firstName','lastName'],
     setup(props , context) {
+        onMounted(function () {
+            console.log('whatever');
+        })
         const uName = computed(function() {
             return props.firstName + ' ' + props.lastName ;
         });
         console.log(context);
         const age=inject('userAge');
+    
         return{userName:uName , age}
     },
 }
